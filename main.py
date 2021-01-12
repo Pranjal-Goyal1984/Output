3fba35f1ef67e8566ca847c81cd9efbb31173513
Pranjal #120976 <120976@eesd.org>
2021-01-12 00:08:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Pranjal Goyal is my name")
+print("I am in 7th grade")
+print("I like the book the Giving Tree")
+print("My favorite superhero is Wanda from Marvel")
+
+# Concatenation is when you add two strings together using '+'
+
